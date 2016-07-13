f488d2139473bce1d2d3233e28e852c50db0431e
Matthew Page <matthewpagecs@gmail.com>
2016-07-13 16:39:38 -0400

    More smooth animations

--- slyce-messaging/src/main/java/it/slyce/messaging/SlyceMessagingFragment.java
+++ slyce-messaging/src/main/java/it/slyce/messaging/SlyceMessagingFragment.java
@@ -261,8 +261,8 @@ public class SlyceMessagingFragment extends Fragment implements OnClickListener 
             Message message = messages.get(i);
             mMessages.add(0, message);
         }
-        // if (moreMessagesExist)
-        //    mMessages.add(0, new SpinnerMessage());
+        if (moreMessagesExist)
+            mMessages.add(0, new SpinnerMessage());
         replaceMessages(mMessages, upTo);
     }
 
