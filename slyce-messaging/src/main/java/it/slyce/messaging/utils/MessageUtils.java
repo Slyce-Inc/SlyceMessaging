16977eb73967c71a0379c449d6583393ba6631df
Audrey Troutt <audrey.troutt@gmail.com>
2016-07-14 15:56:38 -0400

    REFACTOR: extracted methods to make this code easier to understand at a glance

--- slyce-messaging/src/main/java/it/slyce/messaging/utils/MessageUtils.java
+++ slyce-messaging/src/main/java/it/slyce/messaging/utils/MessageUtils.java
@@ -29,19 +29,41 @@ public class MessageUtils {
         }
     }
 
-    private static boolean isFirst(int i, List<MessageItem> messageItems) {
+    private static boolean previousMessageIsNotFromSameSource(int i, List<MessageItem> messageItems) {
         return i == 0 ||
-                messageItems.get(i - 1).getMessageItemType() == MessageItemType.SPINNER ||
-                messageItems.get(i - 1).getMessageSource() != messageItems.get(i).getMessageSource();
+                previousMessageIsSpinner(i, messageItems) ||
+                previousMessageIsFromAnotherSender(i, messageItems);
     }
 
-    private static boolean isLast(int i, List<MessageItem> messageItems) {
-        if (messageItems.get(i).getMessageItemType() == MessageItemType.SPINNER)
+    private static boolean previousMessageIsFromAnotherSender(int i, List<MessageItem> messageItems) {
+        return messageItems.get(i - 1).getMessageSource() != messageItems.get(i).getMessageSource();
+    }
+
+    private static boolean previousMessageIsSpinner(int i, List<MessageItem> messageItems) {
+        return isSpinnerMessage(i - 1, messageItems);
+    }
+
+    private static boolean isTheLastConsecutiveMessageFromSource(int i, List<MessageItem> messageItems) {
+        if (isSpinnerMessage(i, messageItems)) {
             return false;
+        }
+
         return i == messageItems.size() - 1 ||
-                messageItems.get(i).getMessageSource() != messageItems.get(i + 1).getMessageSource() ||
+                nextMessageHasDifferentSource(i, messageItems) ||
                 messageItems.get(i).getMessage() == null ||
                 messageItems.get(i + 1).getMessage() == null ||
-                messageItems.get(i + 1).getMessage().getDate() - messageItems.get(i).getMessage().getDate() > 1000 * 60 * 60; // one hour
+                nextMessageWasAtLeastAnHourAfterThisOne(i, messageItems);
+    }
+
+    private static boolean nextMessageWasAtLeastAnHourAfterThisOne(int i, List<MessageItem> messageItems) {
+        return messageItems.get(i + 1).getMessage().getDate() - messageItems.get(i).getMessage().getDate() > 1000 * 60 * 60; // one hour
+    }
+
+    private static boolean nextMessageHasDifferentSource(int i, List<MessageItem> messageItems) {
+        return messageItems.get(i).getMessageSource() != messageItems.get(i + 1).getMessageSource();
+    }
+
+    private static boolean isSpinnerMessage(int i, List<MessageItem> messageItems) {
+        return messageItems.get(i).getMessageItemType() == MessageItemType.SPINNER;
     }
 }
