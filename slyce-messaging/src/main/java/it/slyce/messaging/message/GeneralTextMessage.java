6118e3697763e843620b0c0ff689bb7d720eb3e0
Matthew Page <matthewpagecs@gmail.com>
2016-08-10 17:16:35 -0400

    added GeneralTextMessage

---
+++ slyce-messaging/src/main/java/it/slyce/messaging/message/GeneralTextMessage.java
@@ -0,0 +1,18 @@
+package it.slyce.messaging.message;
+
+import android.content.Context;
+
+import it.slyce.messaging.message.messageItem.MessageItem;
+
+public class GeneralTextMessage extends Message {
+    private String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public MessageItem toMessageItem(Context context) {
+        return null;
+    }
+}
