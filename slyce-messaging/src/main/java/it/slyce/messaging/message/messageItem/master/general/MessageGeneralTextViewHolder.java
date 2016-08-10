280412b1e44aeb795c8174a991f52082effb8e73
Matthew Page <matthewpagecs@gmail.com>
2016-08-10 18:02:34 -0400

    Added MessageGeneralTextViewHolder

---
+++ slyce-messaging/src/main/java/it/slyce/messaging/message/messageItem/master/general/MessageGeneralTextViewHolder.java
@@ -0,0 +1,20 @@
+package it.slyce.messaging.message.messageItem.master.general;
+
+import android.view.View;
+import android.widget.TextView;
+
+import it.slyce.messaging.message.messageItem.MessageViewHolder;
+import it.slyce.messaging.utils.CustomSettings;
+
+/**
+ * @Author Matthew Page
+ * @Date 8/10/16
+ */
+public class MessageGeneralTextViewHolder extends MessageViewHolder {
+    public TextView messageTextView;
+    public MessageGeneralTextViewHolder(View itemView, CustomSettings customSettings) {
+        super(itemView, customSettings);
+
+        // TODO initialize messageTextView
+    }
+}
