35fc682d8c8d6ae5a4d0107a7df808b9e664212a
Brian Pham <brian@theleagueapp.co>
2016-10-06 18:32:53 -0700

    clickable links getting a fix on text

--- slyce-messaging/src/main/java/it/slyce/messaging/view/text/FontTextView.java
+++ slyce-messaging/src/main/java/it/slyce/messaging/view/text/FontTextView.java
@@ -5,8 +5,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.os.Build;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 import it.slyce.messaging.R;
@@ -68,5 +74,49 @@ public class FontTextView extends TextView {
 
             }
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        TextView widget = (TextView) this;
+        Object text = widget.getText();
+        if (text instanceof Spanned) {
+            Spannable buffer = (Spannable) text;
+
+            int action = event.getAction();
+
+            if (action == MotionEvent.ACTION_UP
+                    || action == MotionEvent.ACTION_DOWN) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+
+                x -= widget.getTotalPaddingLeft();
+                y -= widget.getTotalPaddingTop();
+
+                x += widget.getScrollX();
+                y += widget.getScrollY();
+
+                Layout layout = widget.getLayout();
+                int line = layout.getLineForVertical(y);
+                int off = layout.getOffsetForHorizontal(line, x);
+
+                ClickableSpan[] link = buffer.getSpans(off, off,
+                        ClickableSpan.class);
+
+                if (link.length != 0) {
+                    if (action == MotionEvent.ACTION_UP) {
+                        link[0].onClick(widget);
+                    } else if (action == MotionEvent.ACTION_DOWN) {
+                        Selection.setSelection(buffer,
+                                buffer.getSpanStart(link[0]),
+                                buffer.getSpanEnd(link[0]));
+                    }
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
     }
 }
